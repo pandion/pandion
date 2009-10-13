0837df14d69f848a63b31c8bea125d4770026aef
Sebastiaan Deckers <cbas@pandion.be>
2009-10-13 20:01:56 +0200

    Build script to automatically generate the appcast feed

---
+++ Installer/Appcaster/build_feed.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SETLOCAL
+
+:: TODO: This information should be centralized and automated. Perhaps using Ant or Maven2?
+SET VERSION_MAJOR=2
+SET VERSION_MINOR=6
+
+:: Hudson sets its environment variables but defaults are provided for manual builds.
+IF NOT DEFINED BUILD_NUMBER SET BUILD_NUMBER=0
+IF NOT DEFINED HUDSON_URL SET HUDSON_URL=http://build.pandion.im/
+SET PACKAGE=Pandion_%VERSION_MAJOR%.%VERSION_MINOR%.%BUILD_NUMBER%.msi
+SET LOCAL_FILE=../WiX/%PACKAGE%
+
+CScript.exe //NoLogo appcaster.wsf /path:index.xml /overwrite:false /application:Pandion /author:"Pandion Team" /description:"Pandion software updates feed" /location:http://feeds.feedburner.com/pandionupdates /history:"%HUDSON_URL%" /arguments:"/passive LAUNCHAPP=yes" /changes:"Development build. See http://github.com/pandion/pandion" /info:"%HUDSON_URL%job/%JOB_NAME%/%BUILD_NUMBER%/changes" /mime:application/x-msi /size:"%LOCAL_FILE%" /track:development /version:"%VERSION_MAJOR%.%VERSION_MINOR%.%BUILD_NUMBER%" /url:"http://update.pandion.be/builds/%PACKAGE%"
