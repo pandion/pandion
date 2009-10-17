e9a7fc504b3f12096ef2ead80959c62ead184d18
Sebastiaan Deckers <cbas@pandion.be>
2009-10-17 18:07:35 +0200

    Tuned appcasting script to pull automatically from FeedBurner and trim its length Fixed appcast script mistake in local (non-Hudson) building Appcaster v1.1.0 improvements: - Loading the feed from a URL or local file - Insert new entries at top of the feed - "maxage" argument to prune expired items in a branch - "maxcount" argument to cap the number of entries in a branch

--- Installer/Appcaster/build_feed.bat
+++ Installer/Appcaster/build_feed.bat
@@ -8,7 +8,9 @@ SET VERSION_MINOR=6
 :: Hudson sets its environment variables but defaults are provided for manual builds.
 IF NOT DEFINED BUILD_NUMBER SET BUILD_NUMBER=0
 IF NOT DEFINED HUDSON_URL SET HUDSON_URL=http://build.pandion.im/
+IF NOT DEFINED JOB_NAME SET JOB_NAME=Pandion
 SET PACKAGE=Pandion_%VERSION_MAJOR%.%VERSION_MINOR%.%BUILD_NUMBER%.msi
 SET LOCAL_FILE=../WiX/%PACKAGE%
+SET FEED_LOCATION=http://feeds.feedburner.com/pandionupdates
 
-CScript.exe //NoLogo appcaster.wsf /path:index.xml /overwrite:false /application:Pandion /author:"Pandion Team" /description:"Pandion software updates feed" /location:http://feeds.feedburner.com/pandionupdates /history:"%HUDSON_URL%" /arguments:"/passive LAUNCHAPP=yes" /changes:"Development build" /info:"%HUDSON_URL%job/%JOB_NAME%/%BUILD_NUMBER%/changes" /mime:application/x-msi /size:"%LOCAL_FILE%" /track:development /version:"%VERSION_MAJOR%.%VERSION_MINOR%.%BUILD_NUMBER%" /url:"http://appcast.pandion.be/builds/%PACKAGE%"
+CScript.exe //NoLogo appcaster.wsf /maxcount:25 /source:"%FEED_LOCATION%" /path:index.xml /overwrite:false /application:Pandion /author:"Pandion Team" /description:"Pandion software updates feed" /location:"%FEED_LOCATION%" /history:"%HUDSON_URL%" /arguments:"/passive LAUNCHAPP=yes" /changes:"Development build" /info:"%HUDSON_URL%job/%JOB_NAME%/%BUILD_NUMBER%/changes" /mime:application/x-msi /size:"%LOCAL_FILE%" /track:development /version:"%VERSION_MAJOR%.%VERSION_MINOR%.%BUILD_NUMBER%" /url:"http://appcast.pandion.be/builds/%PACKAGE%"
