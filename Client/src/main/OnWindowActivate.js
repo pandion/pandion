486f3b24f3d8e93282fdc71d90f94728d565a1c9
Sebastiaan Deckers <cbas@pandion.be>
2009-11-08 21:05:04 +0100

    Minor fix for focus issue in contact list search box

--- Client/src/main/OnWindowActivate.js
+++ Client/src/main/OnWindowActivate.js
@@ -1,8 +1,5 @@
-﻿function OnWindowActivate ()
-{
-	if ( document.getElementById( 'content-dialog' ).style.display == "block" )
-	{
-		external.globals( 'ClientRoster' ).Search.Focus();
-		external.globals( 'ClientRoster' ).Search.Blur();
+﻿function OnWindowActivate () {
+	if (document.getElementById("content-dialog").style.display == "block") {
+		external.globals("ClientRoster").Search.Blur();
 	}
 }
