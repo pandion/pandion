83f8f1159dcf6391f47d794b21d8a61180f1b032
Dries Staelens <dries.staelens@gmail.com>
2009-11-10 03:21:24 +0100

    Added IsRectOnMonitor() part2

--- Host/Source/SASL.h
+++ Host/Source/SASL.h
@@ -21,6 +21,7 @@
  */
 #pragma once
 #include "DispInterfaceImpl.h"
+#include "SSPI.h"
 
 #define PACKAGE_NAME	TEXT("Negotiate")
 
@@ -28,14 +29,7 @@ class SASL :
 	public DispInterfaceImpl<ISASL>
 {
 private:
-	DWORD			m_dwMaxTokenSize;
-
-	BOOL			m_fNewConversation;
-	CredHandle		m_hCred;
-	BOOL			m_fHaveCredHandle;
-	SecHandle		m_hCtxt;
-	BOOL			m_fHaveCtxtHandle;
-
+	SSPI m_SSPI;
 public:
 	SASL();
 	~SASL();
@@ -45,11 +39,9 @@ public:
 	STDMETHOD(DigestGenerateResponse)(
 		BSTR username, BSTR realm, BSTR password, BSTR nonce,
 		BSTR cnonce, BSTR digest_uri, BSTR nc, BSTR qop, BSTR *strDigest);
-	STDMETHOD(SSPIReset)();
-	STDMETHOD(SSPIGenerateResponse)(
-		BSTR Challenge, BOOL *Continue, BSTR *Response);
+	STDMETHOD(get_SSPI)(VARIANT* pDispatch);
+	STDMETHOD(get_GSSAPI)(VARIANT* pDispatch);
 
 private:
 	void HexString(const unsigned char *binaryData, char *hexString, int n);
-	void SSPIError(LPWSTR Where, LPWSTR WhenCalling, LPWSTR ErrorMessage);
 };
