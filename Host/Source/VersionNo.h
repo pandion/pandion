9db4ad44a02e394ed5bc4eab3ae86373ff799586
Dries Staelens <dries@pandion.im>
2010-03-31 21:08:46 +0200

    Added automatic version numbering support for pandion.exe

---
+++ Host/Source/VersionNo.h
@@ -0,0 +1,14 @@
+// Will be overridden by Hudson build server
+
+#ifndef FILEVER
+#define FILEVER 2,6,0,0
+#endif
+#ifndef PRODUCTVER
+#define PRODUCTVER 2,6,0,0
+#endif
+#ifndef FILEVERSTR
+#define FILEVERSTR "2,6,0,0\0"
+#endif
+#ifndef PRODUCTVERSTR
+#define PRODUCTVERSTR "2,6,0,0\0"
+#endif
