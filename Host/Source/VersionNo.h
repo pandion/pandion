0d12d03d2d40001d2172f153ede0f4b4390acae2
Sebastiaan Deckers <cbas@pandion.be>
2010-07-16 15:54:50 +0200

    Building the Host executable now uses Visual C++ 2010

--- Host/Source/VersionNo.h
+++ Host/Source/VersionNo.h
@@ -1,14 +1,26 @@
 // Will be overridden by Hudson build server
 
 #ifndef FILEVER
-#define FILEVER 2,6,0,0
+#define FILEVER 1,0,0,0
 #endif
 #ifndef PRODUCTVER
-#define PRODUCTVER 2,6,0,0
+#define PRODUCTVER 1,0,0,0
 #endif
 #ifndef FILEVERSTR
-#define FILEVERSTR "2,6,0,0\0"
+#define FILEVERSTR "1,0,0,0\0"
 #endif
 #ifndef PRODUCTVERSTR
-#define PRODUCTVERSTR "2,6,0,0\0"
+#define PRODUCTVERSTR "1,0,0,0\0"
+#endif
+#ifndef COMMENTS
+#define COMMENTS ""
+#endif
+#ifndef COMPANY
+#define COMPANY ""
+#endif
+#ifndef DESCRIPTION
+#define DESCRIPTION ""
+#endif
+#ifndef COPYRIGHT
+#define COPYRIGHT ""
 #endif
--- Host/build_host.bat
+++ Host/build_host.bat
@@ -5,13 +5,22 @@
 @ECHO OFF
 IF NOT DEFINED BUILD_CONFIG CALL "..\build_config.bat"
 
+:: Prepare Visual C++ 2010
+IF NOT DEFINED VS100COMNTOOLS ECHO Error: Microsoft Visual C++ 2010 (Express or Professional) is required. && EXIT /B 1
+CALL "%VS100COMNTOOLS%\vsvars32.bat"
+IF %ERRORLEVEL% NEQ 0 ECHO Error: Cannot set environment build variables. && EXIT /B 1
+
+:: Set meta information
 ECHO #define FILEVER %VERSION_MAJOR%,%VERSION_MINOR%,%BUILD_NUMBER%,0 > ".\Source\VersionNo.h"
 ECHO #define PRODUCTVER %VERSION_MAJOR%,%VERSION_MINOR%,%BUILD_NUMBER%,0 >> ".\Source\VersionNo.h"
 ECHO #define FILEVERSTR "%VERSION_MAJOR%,%VERSION_MINOR%,%BUILD_NUMBER%,0\0" >> ".\Source\VersionNo.h"
 ECHO #define PRODUCTVERSTR "%VERSION_MAJOR%,%VERSION_MINOR%,%BUILD_NUMBER%,0\0" >> ".\Source\VersionNo.h"
 
-CALL "%VS90COMNTOOLS%\vsvars32.bat"
-IF %ERRORLEVEL% NEQ 0 ECHO Error: Cannot set environment build variables && EXIT /B 1
+ECHO #define COMMENTS "%HOST_COMMENTS%" >> ".\Source\VersionNo.h"
+ECHO #define COMPANY "%HOST_COMPANY%" >> ".\Source\VersionNo.h"
+ECHO #define DESCRIPTION "%HOST_DESCRIPTION%" >> ".\Source\VersionNo.h"
+ECHO #define COPYRIGHT "%HOST_COPYRIGHT%" >> ".\Source\VersionNo.h"
 
-VCBUILD /useenv /rebuild "host.sln" "Release|Win32"
+:: Start the build
+MSBUILD /p:useenv=true /t:rebuild /p:Configuration=Release /p:Platform=Win32 "host.sln"
 IF %ERRORLEVEL% NEQ 0 ECHO Error: Failed to compile Host && EXIT /B 1
